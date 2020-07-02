8c3574eb8b78d5124aeb7d5f0fc1fd559db0ab9f
JGB_t34m <jgb@gmail.com>
2020-07-01 20:47:02 -0400

    add models to admin apps panel

--- app/game_name/models.py
+++ app/game_name/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 
 # Create your models here.
-class NombreDescripcion(models.Model):
-    nombre = models.CharField(max_length=50, blank=True, null=True)
-    sexo = models.CharField(max_length=50, blank=True, null=True)
-    descripcion = models.TextField(blank=True, null=True)
-
-    class Meta:
-        db_table = 'nombre_descripcion'
-
-
 class DescripcionNombres(models.Model):
     descripcion = models.TextField(blank=True, null=True)
 
     class Meta:
         db_table = 'descripcion_nombres'
+
+
+class NombreDescripcion(models.Model):
+    nombre = models.CharField(max_length=50, blank=True, null=True)
+    sexo = models.CharField(max_length=50, blank=True, null=True)
+    descripcion = models.ForeignKey('DescripcionNombres', models.CASCADE, db_column='descripion', blank=True, null=True)
+
+    class Meta:
+        db_table = 'nombre_descripcion'
--- app/game_tarot/admin.py
+++ app/game_tarot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Carta)
+admin.site.register(CategoriaBasica)
+admin.site.register(Color)
+admin.site.register(Descripciones)
+admin.site.register(TipoArcano)
+admin.site.register(Numero)
